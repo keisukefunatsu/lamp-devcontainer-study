b142d7cec70b85d655a221414eec957cc752042e
keisukefunatsu <keisukeyan2012@gmail.com>
2022-07-17 09:21:38 +0900

    add mysql client and test page

---
+++ cake_sample/templates/Pages/test_page.php
@@ -0,0 +1,38 @@
+<?php
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <?= $this->Html->charset() ?>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>
+        CakePHP: the rapid development PHP framework:
+        <?= $this->fetch('title') ?>
+    </title>
+    <?= $this->Html->meta('icon') ?>
+
+    <link href="https://fonts.googleapis.com/css?family=Raleway:400,700" rel="stylesheet">
+
+    <?= $this->Html->css(['normalize.min', 'milligram.min', 'cake', 'home']) ?>
+
+    <?= $this->fetch('meta') ?>
+    <?= $this->fetch('css') ?>
+    <?= $this->fetch('script') ?>
+</head>
+<body>
+    <header>
+        <div class="container text-center">
+            <h1>
+                This is test page
+            </h1>
+        </div>
+    </header>
+    <main class="main">
+        <div class="container">
+            <p>
+                This is test page
+            </p>
+        </div>
+    </main>
+</body>
+</html>
